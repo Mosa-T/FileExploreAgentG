f1b98c478837a19b77fc86a3fa3a0424c28015e4
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2022-01-25 21:09:46 +0200

    Update FileExploreAgent.java

--- FileExploreAgent.java
+++ FileExploreAgent.java
@@ -8,49 +8,43 @@ public class FileExploreAgent {
 
 
     public static void main(String[] args) throws IOException {
-        String url = "http://localhost:8080/job/FileExplorer//buildWithParameters?token=115072a888c151f50f6e01cee3f1d0ea1b&Hostname=Ayyy&dir=D:/study/logic";
-        URL obj = new URL(url);
+        //String url = "http://localhost:8080/job/FileExplorer//buildWithParameters?token=115072a888c151f50f6e01cee3f1d0ea1b&Hostname=Ayyy&dir=D:/study/logic";
+        //URL obj = new URL(url);
+        //HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        //System.out.println(con.getResponseCode());
+        //con.disconnect();
 
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        System.out.println(con.getResponseCode());
-        con.disconnect();
+        try {
+            
+            URL myURL = new URL("http://localhost:8080/job/FileExplorer/buildWithParameters");
+            String user = "Musa-T"; // username
+            String pass = "115072a888c151f50f6e01cee3f1d0ea1b"; // password or API token
+            String authStr = user + ":" + pass;
+            String encoding = Base64.getEncoder().encodeToString(authStr.getBytes("utf-8"));
+            System.out.println("1");
+            HttpURLConnection connection = (HttpURLConnection) myURL.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Authorization", "Basic " + encoding);
 
-//        try {
-//
-//
-//
-//
-//
-//
-//        URL myURL = new URL("http://localhost:8080/job/FileExplorer/buildWithParameters");
-//        String user = "Musa-T"; // username
-//            String pass = "115072a888c151f50f6e01cee3f1d0ea1b"; // password or API token
-//            String authStr = user + ":" + pass;
-//            String encoding = Base64.getEncoder().encodeToString(authStr.getBytes("utf-8"));
-//            System.out.println("1");
-//            HttpURLConnection connection = (HttpURLConnection) myURL.openConnection();
-//            connection.setRequestMethod("POST");
-//            connection.setDoOutput(true);
-//            connection.setRequestProperty("Authorization", "Basic " + encoding);
-//
-//            System.out.println("2");
-//            String urlParams = "Hostname=Explorer_Agent1&dir=D:/study/logic";
-//            byte[] postData = urlParams.getBytes("utf-8");
-//            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-//                wr.write(postData);
-//            }
-//            System.out.println("3");
-//            //InputStream content = connection.getInputStream();
-//            //BufferedReader in = new BufferedReader(new InputStreamReader(content));
-//            //String line;
-//            //while ((line = in.readLine()) != null) {
-//            //    System.out.println(line);
-//            //}
-//        }
-//        catch (IOException e) {
-//            System.out.println(e);
-//            // openConnection() failed
-//            // ...
-//        }
+            System.out.println("2");
+            String urlParams = "Hostname=Explorer_Agent1&dir=D:/study/logic";
+            byte[] postData = urlParams.getBytes("utf-8");
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.write(postData);
+            }
+            System.out.println("3");
+            //InputStream content = connection.getInputStream();
+            //BufferedReader in = new BufferedReader(new InputStreamReader(content));
+            //String line;
+            //while ((line = in.readLine()) != null) {
+            //    System.out.println(line);
+            //}
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            // openConnection() failed
+            // ...
+        }
     }
 }
